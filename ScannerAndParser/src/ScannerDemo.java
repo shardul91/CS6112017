9ca7a94eb9203890b549d51219a856297857dd24
shardul virkar <shardul91@gmail.com>
2017-11-27 21:26:42 -0500

    Add files via upload

---
+++ ScannerAndParser/src/ScannerDemo.java
@@ -0,0 +1,25 @@
+/**
+ * @author Christelle
+ * 
+ */
+public class ScannerDemo {
+
+	private static String file1 = "prog1.jay";
+	private static int counter = 1;
+
+	public static void main(String args[]) {
+
+		TokenStream ts = new TokenStream(file1);
+
+		System.out.println("Scanning file :" + file1);
+
+		while (!ts.isEndofFile()) {
+			// TO BE COMPLETEDs
+			Token t = ts.nextToken();
+			System.out.println("Token "+ String.format("%02d", counter) +" - Type:" + t.getType() +" - Value:"+ t.getValue());
+			counter = counter + 1;
+
+		}
+		
+	}
+}
